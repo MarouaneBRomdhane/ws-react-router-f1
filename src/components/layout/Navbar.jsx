7417e12bc4086ffb219a23a6c2e5c47a6cdb584f
chihebgmc <chihebchabougmc@gmail.com>
2023-12-13 12:39:43 +0100

    first commit

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <a href="/home">Home</a>
+      <a href="/about">About</a>
+      <a href="/users">Users</a>
+    </nav>
+  );
+};
+
+export default Navbar;
